98c7c507b65c1f00b3c0c3b8d2bccfc4a65764e1
chato1337 <chato1337@gmail.com>
2019-08-04 21:57:56 -0500

    implementacion de sweetAlert2

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -23,6 +23,30 @@ var app = new Vue({
             console.log(content);
             // var numero = parseInt(content);
             me.estudiante = content;
+            let timerInterval
+            Swal.fire({
+              type: 'success',
+              title: 'Registrado en la lista!',
+              html: 'Se aviso se cierra en: <strong></strong> segundos.',
+              timer: 3000,
+              onBeforeOpen: () => {
+                Swal.showLoading()
+                timerInterval = setInterval(() => {
+                  Swal.getContent().querySelector('strong')
+                    .textContent = Swal.getTimerLeft()
+                }, 100)
+              },
+              onClose: () => {
+                clearInterval(timerInterval)
+              }
+            }).then((result) => {
+              if (
+                // Read more about handling dismissals
+                result.dismiss === Swal.DismissReason.timer
+              ) {
+                console.log('I was closed by the timer')
+              }
+            })
             me.pasarLista();
           });
           Instascan.Camera.getCameras().then(function (cameras) {
